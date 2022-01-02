37be72cba5c5df6c8128a8c0ac5979e9dcdc99f0
Chris <cevans@outreach.com>
2022-01-01 20:19:53 -0700

    Finish chapter 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement("h1", {style: {color:'blue'}}, "Heyyy!"),
   document.getElementById('root')
 );
 
