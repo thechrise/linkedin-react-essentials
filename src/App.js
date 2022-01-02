c4f6b82be13abe303ebdc69bf35b2497cbb364e2
Chris <cevans@outreach.com>
2022-01-01 21:42:32 -0700

    Introducing react testing library

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-//https://api.github.com/users/thechrise
-
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
-      .then(setData)
-      .catch(setError)
-      .finally(setLoading(false));
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-
-  if (!data) return null;
-
-  if (data) {
+function App() {
     return (
       <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
+        <h1>Hello React Testing Library</h1>
       </div>
-    );
-  }
+    )
 }
 
 export default App;
